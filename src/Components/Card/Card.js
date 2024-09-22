946f3237dc02b055fa645411beb32ddd67e000ed
joaquinaedo <jedo@udesa.edu.ar>
2024-09-22 19:13:38 -0300

    Boton de agregar a favoritos

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -9,11 +9,53 @@ class Card extends Component{
 
         this.state = {
             pelicula: null, 
+            esFavorito: false
+        } 
+    };
+    
+    componentDidMount() { 
+    const storage = localStorage.getItem("favoritos") 
+    if (storage !== null) {
+    const parsedStorage = JSON.parse(storage); 
+    const estaEnFavoritos = parsedStorage.includes (this.props.pelicula.id)
+    
+    if(estaEnFavoritos){
+    this.setState({
+    esFavorito:true
+    })
+    }};
+}
+    
+    agregarAFavoritos(){
+    
+    const storage = localStorage.getItem("favoritos") 
+    if (storage !== null) {
+    const parsedStorage = JSON.parse(storage); 
+    parsedStorage.push(this.props.pelicula.id)
+    const stringStorage = JSON.stringify(parsedStorage) 
+    localStorage.setItem("favoritos", stringStorage)    
+    } 
+    else{
+    const primerFavorito = [this.props.pelicula.id] 
+    const stringStorage = JSON.stringify(primerFavorito) 
+    localStorage.setItem("favoritos", stringStorage )
+    } 
 
-        }
-        
-        
+    this.setState({
+    esFavorito: true})
     }
+    
+    quitarDeFavoritos(){
+    const storage = localStorage.getItem("favoritos") 
+    const parsedStorage = JSON.parse(storage); 
+    const restoFavoritos = parsedStorage.filter((id) => id !== this.props.pelicula.id)
+    const stringStorage = JSON.stringify(restoFavoritos) 
+    localStorage.setItem("favoritos", stringStorage)
+    this.setState({
+        esFavorito: false,
+    });
+    } 
+    
     componentDidMount(){ 
         fetch(`https://api.themoviedb.org/3/movie/${this.props.id}?api_key=31e421d77201e7a1eefe33f85b67fa3b`)
 
@@ -28,7 +70,7 @@ class Card extends Component{
             )
         })
     
-        console.log(this.pelicula)
+        console.log(this.state.pelicula)
 
         
 
@@ -43,7 +85,7 @@ class Card extends Component{
                 <h3>{this.state.pelicula.original_title}</h3>
                 <p className='descripcion'>{this.state.pelicula.overview} </p>
                 <a href=''>Ir a detalles</a>
-                <p>AGRRGAR A FAVORTITOS</p>
+                <button onClick={() => !this.state.esFavorito} > {!this.state.esFavorito ? "Agregar a favoritos" : "Quitar de favoritos"} Agregar a favoritos</button>
             </div>
             )
         }}
