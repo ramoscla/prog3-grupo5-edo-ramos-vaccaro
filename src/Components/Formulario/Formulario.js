65209b1fa1854f3880b7ce690f952dc91b94e920
joaquinaedo <jedo@udesa.edu.ar>
2024-09-22 16:04:21 -0300

    ...

---
+++ src/Components/Formulario/Formulario.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Formulario extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {valor: ''};
+    }
+  
+    evitarSubmit(event) {
+      event.preventDefault();
+    }
+  
+    controlarCambios(event) {
+      this.setState({valor: event.target.value});
+    }
+  
+    render() {
+      return (
+       <form onSubmit={(event)=>this.evitarSubmit(event)}>
+         <label>Name:</label>
+         <input type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} />
+         <input type="submit" value="Submit" />
+       </form>
+      );
+    }
+  }
+
+  export default Formulario; 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Navbar from "./Navbar/Navbar"
-
+import Navbar from "../Navbar/Navbar"
 
 const Header = () => {
-
+    return (
     <div>
          <img src="./img/logo.png" alt="logo"></img>
         <Navbar/>
-
+    
     </div>
-
+    )
 
 
     
