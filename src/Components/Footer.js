59434deff43bda7873ac6bb2d802a01fcf455de4
joaquinaedo <jedo@udesa.edu.ar>
2024-09-22 14:01:26 -0300

    Creación carpeta components y header y footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer>
+            <ul> 
+                <li>
+                    Joaquina  Edo 
+                </li>
+                <li>
+                    Natalia Vaccaro Bianchi
+                </li>
+                <li>
+                    Clara Emilia Ramos 
+                </li>
+            </ul>
+       
+    </footer>
+    )
+}
+
+export default Footer;
