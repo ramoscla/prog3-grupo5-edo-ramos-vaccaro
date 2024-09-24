a06fe0d15b2a3ccd3a737c81230a8c3a522a0c94
natalia vaccaro <nvaccarobianchi@udesa.edu.ar>
2024-09-24 10:44:38 -0300

    emprolijado de card y funcion favoritos + modificacion rutas

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Favorites from './Components/Pages/Favorites';
 import Detalle from './Components/Pages/Detalle';
+import TopRated from "./Components/Pages/TopRated"
 
 function App() {
   return (
     <>
-   
- 
-    <Header/>
-    <Switch>
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/favoritos" component={Favorites} />
-      <Route path="/detalle/:id" component={Detalle} />  
-    {/*  />
-     <Route path="/proximamente" component={} />
-     <Route path="/cartelera" component={} />
-     */}
-    </Switch>
 
-    <Footer/>
+
+      <Header />
+
+      <Switch>
+
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/favoritos" component={Favorites} />
+        <Route path="/detalle/:id" component={Detalle} />
+        <Route path="/top" component={TopRated} />
+        <Route path="/populares" component={Home} />
+
+      </Switch>
+      
+      <Footer />
     </>
 
 
