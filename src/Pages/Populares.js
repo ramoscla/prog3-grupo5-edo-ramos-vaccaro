320eda8b59079d9ea8b3f0801330b940918b0b53
ramoscla <ceramos@udesa.edu.ar>
2024-09-24 13:03:25 -0300

    modifiqué la lógica de CardGridSinSlice y agregué la funcionalidad de cargar más a la página Populares

--- src/Pages/Populares.js
+++ src/Pages/Populares.js
@@ -1,20 +1,51 @@
 import React, { Component } from "react";
 import CardGridSinSlice from "../Components/CardGridSinSlice/CardGridSinSlice";
 
-
-const apiKey = "5d038daa07630bd00fca08f5408cb116"
-const apiPopulares = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+const apiKey = "5d038daa07630bd00fca08f5408cb116";
+const apiPopulares = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=`;
 
 class Populares extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            peliculas: [],
+            actualPage: 1,
+        };
+    }
 
-    render(){
+    componentDidMount(){
+        fetch(`${apiPopulares}${this.state.actualPage}`)
+            .then((response) => response.json())
+            .then(data =>
+                this.setState({
+                    peliculas: data.results,
+                    actualPage: this.state.actualPage + 1,
+                })
+            )
+            .catch((error) => console.log(error));
+    }
 
-        return(
-           <div className="Populares">
-            <CardGridSinSlice url={apiPopulares} title="Populares" /> 
-           </div>
-        )
-        
-    }}
-    
-    export default Populares
+    handleLoadMore(){
+        fetch(`${apiPopulares}${this.state.actualPage}`)
+            .then((response) => response.json())
+            .then((data) =>
+                this.setState({
+                    peliculas: this.state.peliculas.concat(data.results),
+                    actualPage: this.state.actualPage + 1,
+                })
+            )
+            .catch((error) => console.log(error));
+    };
+
+    render() {
+        return (
+            <div className="Populares">
+                <CardGridSinSlice arrayPeliculas={this.state.peliculas} title="Populares" />
+                <button onClick={()=> this.handleLoadMore()}>Cargar más</button>
+            </div>
+        );
+    }
+}
+
+export default Populares;
+
